929ff1f3dd13ff855f4b891eb05c16e9c38e405d
Asif Mehmood <asif-mehmood@hotmail.com>
2022-12-07 20:44:09 +0500

    list item button

--- src/components/appbar/appbarDesktop.jsx
+++ src/components/appbar/appbarDesktop.jsx
@@ -1,7 +1,7 @@
 import { ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
 import { AppbarContainer, AppbarHeader, MyList } from "../../styles/appbar";
 import SearchIcon from "@mui/icons-material/Search";
-
+import { Link } from "react-router-dom";
 import Actions from "./actions";
 import { useUIContext } from "../../context/ui";
 
@@ -12,11 +12,23 @@ export default function AppbarDesktop({ matches }) {
     <AppbarContainer>
       <AppbarHeader variant="h4">Sab One Dollar</AppbarHeader>
       <MyList type="row">
-        <ListItemText primary="Home" />
+        <ListItemButton button component={Link} to="/">
+          Home
+        </ListItemButton>
+        <ListItemButton button component={Link} to="/products">
+          Products
+        </ListItemButton>
+        <ListItemButton button component={Link} to="/aboutus">
+          About us
+        </ListItemButton>
+        <ListItemButton button component={Link} to="/login">
+          Logout
+        </ListItemButton>
+        {/* <ListItemText primary="Home" />
         <ListItemText primary="Categories" />
         <ListItemText primary="Products" />
         <ListItemText primary="About us" />
-        <ListItemText primary="Contact us" />
+        <ListItemText primary="Contact us" /> */}
         <ListItemButton onClick={() => setShowSearchBox(true)}>
           <ListItemIcon>
             <SearchIcon />
