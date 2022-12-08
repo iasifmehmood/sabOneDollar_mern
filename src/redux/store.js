f496e71a04ca60ae2763d6f951f3306026e49c86
Asif Mehmood <asif-mehmood@hotmail.com>
2022-12-08 21:18:49 +0500

    working on cart 9:18pm

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,16 +2,16 @@ import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-//import { cartReducer } from "./reducers/cartReducer";
+import { cartReducer } from "./reducers/cartReducer";
 import {
   getProductDetailsReducer,
   getProductReducer,
 } from "./reducers/productReducer";
 
 const reducer = combineReducers({
-  //   cart: cartReducer,
   getProducts: getProductReducer,
   getProductDetails: getProductDetailsReducer,
+  cart: cartReducer,
 });
 
 const middleware = [thunk];
